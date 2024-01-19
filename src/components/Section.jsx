8bab36021910d539650c20ecb13e29211075dea9
Mutuba <danielmutubait@gmail.com>
2024-01-19 15:30:47 +0300

    Prevent XSS attacks by sanitizing text area input

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import DOMPurify from "dompurify";
+
 import "../App.css";
 
 const Section = ({ text, setText, ssml, setSSML }) => {
+  const sanitizedText = DOMPurify.sanitize(text);
+
   return (
     <div className="section-container">
       <textarea
-        value={text}
+        value={sanitizedText}
         style={{ padding: 15 }}
         placeholder="Write your text to convert. After converting click the play button below to play."
         onChange={(e) => setText(e.target.value)}
