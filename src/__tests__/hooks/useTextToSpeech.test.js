2f577aa5b2f944e62005b3d91049126099ee6dba
Mutuba <danielmutubait@gmail.com>
2024-01-28 16:06:52 +0300

    Finish tests for useTextToSpeech

--- src/__tests__/hooks/useTextToSpeech.test.js
+++ src/__tests__/hooks/useTextToSpeech.test.js
@@ -36,61 +36,26 @@ describe("useTextToSpeech", () => {
       expect(response).toBe(successResult);
     });
   });
+
+  test("useTextToSpeech handles error when synthesizing speech", async () => {
+    const failResult = new Error("Speech synthesis failed");
+
+    AWSMock.mock("Polly", "synthesizeSpeech", (params, callback) => {
+      expect(params).toEqual({
+        OutputFormat: "mp3",
+        Text: "Test text",
+        VoiceId: "Salli",
+      });
+      return callback(failResult);
+    });
+    const { result } = renderHook(() => useTextToSpeech());
+    const { convertTextToSpeech, loading } = result.current;
+
+    await act(async () => {
+      const response = await convertTextToSpeech("Test text", false);
+      expect(response).toBe(failResult);
+    });
+
+    expect(loading).toBeFalsy();
+  });
 });
-
-// import AWSMock from "aws-sdk-mock";
-// import AWS from "aws-sdk";
-// import { renderHook, act } from "@testing-library/react";
-// import useTextToSpeech from "../../hooks/useTextToSpeech";
-
-// describe("Test Delete S3 object", () => {
-//   beforeEach(() => {
-//     AWSMock.setSDKInstance(AWS);
-//   });
-
-//   afterEach(() => {
-//     AWSMock.restore("Polly");
-//   });
-
-//   it("Should be successfully completed", async () => {
-//     const successResult = { success: true };
-//     AWSMock.mock("Polly", "synthesizeSpeech", (params, callback) => {
-//       expect(params).toEqual({
-//         OutputFormat: "mp3",
-//         Text: "Test text",
-//         VoiceId: "Salli",
-//       });
-//       return callback(null, successResult);
-//     });
-
-//     const paramsTest = "Test text";
-//     const { result } = renderHook(() => useTextToSpeech());
-//     const { convertTextToSpeech } = result.current;
-
-//     await act(async () => {
-//       const finalResponse = await convertTextToSpeech(paramsTest);
-//       expect(finalResponse).toBe(successResult);
-//     });
-//   });
-
-//   it("Should fail", async () => {
-//     const failResult = { success: false };
-//     AWSMock.mock("Polly", "synthesizeSpeech", (params, callback) => {
-//       expect(params).toEqual({
-//         OutputFormat: "mp3",
-//         Text: "Test text",
-//         VoiceId: "Salli",
-//       });
-//       return callback(failResult);
-//     });
-
-//     const paramsTest = "Test text";
-//     const { result } = renderHook(() => useTextToSpeech());
-//     const { convertTextToSpeech } = result.current;
-
-//     await act(async () => {
-//       const finalResponse = await convertTextToSpeech(paramsTest);
-//       expect(finalResponse).toEqual(failResult);
-//     });
-//   });
-// });
